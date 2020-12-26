ca0c7997376c1b8d06f8b9316924b51f4b5df25a
freeblendertube <freeblendertube@gmail.com>
2020-12-26 12:21:37 -0500

    added hyvor

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 # Run this scrript in the root directory of your Jekyll source
 # repository for your GitHub page, and it will commit and push
-# the source to origin/source, then build, commit and push the 
+# the source to origin/source, then build, commit and push the
 # resuling HTML to your origin/master, i.e. your website. The
-# commit message for both commits is given by the first 
+# commit message for both commits is given by the first
 # argument.
 
 if [[ -z "$1" ]]; then
